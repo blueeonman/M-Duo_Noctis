b12cda3ba35afbdd9895f1bf255ae445dada10bf
Nguyen Nguyen <nnguy5@gmu.edu>
2023-04-15 23:05:50 -0400

    Respawn work

--- Assets/KillPlayer.cs
+++ Assets/KillPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KillPlayer : MonoBehaviour
 {
@@ -23,6 +24,8 @@ public class KillPlayer : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
             player.transform.position = respawnPoint.position;
         }
     }
