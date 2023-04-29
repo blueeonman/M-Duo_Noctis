f1659e9f0395a767ad42f43fdab1bb4f44936f70
Nguyen Nguyen <nnguy5@gmu.edu>
2023-04-28 23:32:56 -0400

    Rework Grapple hook

--- Assets/Scripts/GrappleHook.cs
+++ Assets/Scripts/GrappleHook.cs
@@ -5,48 +5,11 @@ using UnityEngine;
 public class GrappleHook : MonoBehaviour
 {
 
-    public GameObject hook;
-    public GameObject hookHolder;
+    LineRenderer line;
 
-    public float hookTravelSpeed;
-    public float playerTravelSpeed;
 
-    public static bool fired;
-    public bool hooked;
-
-    public float maxDistance;
-    private float currentDistance;
-
-    void Update()
-    {
-        //firing the hook
-        if (Input.GetKeyDown(KeyCode.Space) && fired == false)
-            fired = true;
-
-        if (fired == true && hooked == false)
-        {
-            hook.transform.Translate(Vector3.forward * Time.deltaTime * hookTravelSpeed);
-            currentDistance = Vector3.Distance(transform.position, hook.transform.position);
-
-            if (currentDistance <= maxDistance)
-                ReturnHook();
-        }
-
-        if (hooked == true)
-        {
-            transform.position = Vector3.MoveTowards(transform.position,
-            hook.transform.position, Time.deltaTime * playerTravelSpeed);
-            float distanceToHook = Vector3.Distance(transform.position, hook.transform.position);
-
-            if (distanceToHook > 1)
-                ReturnHook();
-        }
-    }
-
-    void ReturnHook()
-    {
-        hook.transform.position = hookHolder.transform.position;
-        fired = false;
-        hooked = false;
-    }
+    [SerializeField] LayerMask grapplableMask;
+    [SerializeField] float maxDistance = 10f;
+    [SerializeField] float grappleSpeed = 10f;
+    [SerializeField] float grappleShootSpeed = 20f;
 }
