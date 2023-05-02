0c99db6043a602411a0e1f3ee372b1dccdb06273
blueeonman <blueeonman@gmail.com>
2023-05-02 18:57:48 -0400

    re-added work from last night
    i re-added my scripts. i s2g if they're gone again

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    
+    public Animator animator;
+    public Transform attackpoint;
+    public LayerMask enemyLayers;
+    
+    public float attackRange = 0.5f;
+    public int attackDamage = 50;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;  
+            }
+        }
+    }
+
+    void Attack()
+    {
+        //play an attack animation
+        animator.SetTrigger("Attack");
+
+        //detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackpoint.position, attackRange, enemyLayers);
+
+        //Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<MeleeEnemy>().TakeDamage(attackDamage);
+            Debug.Log(this.gameObject.name + "hit" + enemy.name);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (attackpoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackpoint.position, attackRange);
+    }
+}
