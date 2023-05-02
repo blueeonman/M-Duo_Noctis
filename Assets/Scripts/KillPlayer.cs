899ccb476b2651950ef5ccd23ca3d8782f4e2a56
Gabriel Lea <129460792+Gabriel-Lea@users.noreply.github.com>
2023-05-02 13:15:10 -0400

    Fixed a repository issue

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnPoint;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+            player.transform.position = respawnPoint.position;
+        }
+    }
+}
